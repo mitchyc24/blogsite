24d53c3da93c62be4cc0c2959675fad95fef86bd
Mitchell Carroll <mitchell.carroll24@gmail.com>
2023-01-04 13:07:42 -0500

    Blog posting

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
 
 def home(request):
     return render(request, "blog/home.html", context={"posts": Post.objects.all()})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title","content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["title","content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, "blog/about.html", context={"title": "About"})
