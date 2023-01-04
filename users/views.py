2911b8f4d7d299fed30d4bb6ff369a6754aee496
Mitchell Carroll <mitchell.carroll24@gmail.com>
2023-01-03 21:11:12 -0500

    Large Commit. Hope it works

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+       
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Succesfully created account for {username}")
+            
+            authenticated_user = authenticate(username=user.username, password=request.POST["password1"])
+            login(request, authenticated_user)
+            return redirect('blog-home')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
+
