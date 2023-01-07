6a34338c405dfa960754aac33828bc6d6bc9c87a
Mitchell Carroll <mitchell.carroll24@gmail.com>
2023-01-07 16:14:52 -0500

    password reset

--- django_site/settings.py
+++ django_site/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -126,3 +126,4 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username + "'s Profile"
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             img.thumbnail((300,300))
