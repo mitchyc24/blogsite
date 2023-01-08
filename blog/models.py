7e162897c98d0c5d6ef2faa0db461ca1006bc8bf
Mitchell Carroll <mitchell.carroll24@gmail.com>
2023-01-08 18:55:30 -0500

    Last update before flush

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.urls import reverse
+from PIL import Image
+
+
 
 
 class Post(models.Model):
@@ -9,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="post_images/", blank=True, null=True)
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True, max_length=300)
 
     def __str__(self):
         return self.title
@@ -17,4 +21,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk":self.pk})
 
-
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                img.thumbnail((300,300))
+                img.save(self.image.path)
